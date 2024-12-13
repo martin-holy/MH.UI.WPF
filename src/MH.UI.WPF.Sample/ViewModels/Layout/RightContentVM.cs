64599f42e351c51d912baeac81b1699bd1270813
martin-holy <martin.holy@hotmail.com>
2024-12-13 11:25:37 +0100

    [C] TreeView ItemSelectedEvent

--- src/MH.UI.WPF.Sample/ViewModels/Layout/RightContentVM.cs
+++ src/MH.UI.WPF.Sample/ViewModels/Layout/RightContentVM.cs
@@ -13,6 +13,6 @@ public class RightContentVM : ObservableObject {
   public SlidePanelPinButton SlidePanelPinButton { get; } = new();
 
   public RightContentVM() {
-    FolderTreeView.ItemSelectedEvent += (_, e) => SelectedFolder = e.Data;
+    FolderTreeView.ItemSelectedEvent += (_, item) => SelectedFolder = item;
   }
 }
