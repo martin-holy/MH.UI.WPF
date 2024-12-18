6894cd57f6ebaa628aea51ea24262db994ffb0b3
martin-holy <martin.holy@hotmail.com>
2024-12-18 11:22:27 +0100

    [N] ThicknessExtensions: FromThicknessD

---
+++ src/MH.UI.WPF/Extensions/ThicknessExtensions.cs
@@ -0,0 +1,9 @@
+﻿using MH.Utils.Types;
+using System.Windows;
+
+namespace MH.UI.WPF.Extensions;
+
+public static class ThicknessExtensions {
+  public static Thickness FromThicknessD(this ThicknessD thickness) =>
+    new(thickness.Left, thickness.Top, thickness.Right, thickness.Bottom);
+}
