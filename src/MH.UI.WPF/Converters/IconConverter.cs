b31f28520f58f72c7306e542cdd6be693d7704e7
martin-holy <martin.holy@hotmail.com>
2025-06-09 15:14:17 +0200

    IconConverter

---
+++ src/MH.UI.WPF/Converters/IconConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace MH.UI.WPF.Converters;
+
+public class IconConverter : BaseConverter {
+  private static readonly object _lock = new();
+  private static IconConverter? _inst;
+  public static IconConverter Inst { get { lock (_lock) { return _inst ??= new(); } } }
+
+  public override object? Convert(object? value, object? parameter) =>
+    value is string iconName
+    ? RelayCommandConverter.GetIcon(iconName)
+    : Binding.DoNothing;
+}
