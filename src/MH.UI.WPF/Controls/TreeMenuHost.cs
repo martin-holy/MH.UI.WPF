5990750a5500c743051581adde8916f8fdba651b
martin-holy <martin.holy@hotmail.com>
2025-10-24 13:20:06 +0200

    [N] TreeMenuHost

---
+++ src/MH.UI.WPF/Controls/TreeMenuHost.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MH.UI.WPF.Controls;
+
+public class TreeMenuHost : Control {
+  public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+    nameof(Icon), typeof(object), typeof(TreeMenuHost));
+
+  public static readonly DependencyProperty MenuProperty = DependencyProperty.Register(
+    nameof(Menu), typeof(IEnumerable<MH.Utils.BaseClasses.MenuItem>), typeof(TreeMenuHost));
+
+  public object Icon {
+    get => GetValue(IconProperty);
+    set => SetValue(IconProperty, value);
+  }
+
+  public IEnumerable<MH.Utils.BaseClasses.MenuItem> Menu {
+    get => (IEnumerable<MH.Utils.BaseClasses.MenuItem>)GetValue(MenuProperty);
+    set => SetValue(MenuProperty, value);
+  }
+}
