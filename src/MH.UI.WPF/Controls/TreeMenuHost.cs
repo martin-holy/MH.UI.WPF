59d2470621e5cedecfd353c614f1f1e4e644d781
martin-holy <martin.holy@hotmail.com>
2026-03-06 16:43:33 +0100

    [U] TreeMenuHost: MenuFactory

--- src/MH.UI.WPF/Controls/TreeMenuHost.cs
+++ src/MH.UI.WPF/Controls/TreeMenuHost.cs
@@ -1,4 +1,5 @@
 ﻿using MH.Utils.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,36 +10,42 @@ public class TreeMenuHost : Control {
   public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
     nameof(Icon), typeof(string), typeof(TreeMenuHost));
 
-  public static readonly DependencyProperty MenuProperty = DependencyProperty.Register(
-    nameof(Menu), typeof(IEnumerable<ITreeItem>), typeof(TreeMenuHost));
+  private static readonly DependencyPropertyKey MenuPropertyKey = DependencyProperty.RegisterReadOnly(
+    nameof(Menu), typeof(IEnumerable<ITreeItem>), typeof(TreeMenuHost),
+    new PropertyMetadata(new ITreeItem[] { new MH.Utils.BaseClasses.TreeItem() }));
 
-  public static readonly DependencyProperty MenuPropertyNameProperty = DependencyProperty.Register(
-    nameof(MenuPropertyName), typeof(string), typeof(TreeMenuHost));
+  public static readonly DependencyProperty MenuProperty = MenuPropertyKey.DependencyProperty;
+
+  public static readonly DependencyProperty MenuFactoryProperty = DependencyProperty.Register(
+    nameof(MenuFactory), typeof(Func<IEnumerable<ITreeItem>>), typeof(TreeMenuHost));
 
   public string Icon {
     get => (string)GetValue(IconProperty);
     set => SetValue(IconProperty, value);
   }
 
-  public IEnumerable<ITreeItem> Menu {
-    get => (IEnumerable<ITreeItem>)GetValue(MenuProperty);
-    set => SetValue(MenuProperty, value);
+  public IEnumerable<ITreeItem>? Menu {
+    get => (IEnumerable<ITreeItem>?)GetValue(MenuProperty);
+    private set => SetValue(MenuPropertyKey, value);
   }
 
-  public string? MenuPropertyName {
-    get => (string?)GetValue(MenuPropertyNameProperty);
-    set => SetValue(MenuPropertyNameProperty, value);
+  public Func<IEnumerable<ITreeItem>>? MenuFactory {
+    get => (Func<IEnumerable<ITreeItem>>?)GetValue(MenuFactoryProperty);
+    set => SetValue(MenuFactoryProperty, value);
   }
+
+  private MenuItem? _rootMenuItem;
 
   public override void OnApplyTemplate() {
     base.OnApplyTemplate();
 
-    if (GetTemplateChild("PART_RootMenuItem") is MenuItem rootMenuItem)
-      rootMenuItem.SubmenuOpened += _onMenuOpened;
+    if (_rootMenuItem != null) _rootMenuItem.SubmenuOpened -= _onMenuOpened;
+    _rootMenuItem = GetTemplateChild("PART_RootMenuItem") as MenuItem;
+    if (_rootMenuItem != null) _rootMenuItem.SubmenuOpened += _onMenuOpened;
   }
 
   private void _onMenuOpened(object sender, RoutedEventArgs e) {
-    if (DataContext is MH.Utils.BaseClasses.ObservableObject o && MenuPropertyName != null)
-      o.OnPropertyChanged(MenuPropertyName);
+    if (sender != e.OriginalSource) return;
+    if (MenuFactory != null) Menu = MenuFactory();
   }
 }
