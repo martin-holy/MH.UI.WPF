655c3b931bcf36dc1c92241f82715bfd11a91a25
martin-holy <martin.holy@hotmail.com>
2026-03-25 13:42:18 +0100

    [N] VisibilityConverter: TypeToVisible

--- src/MH.UI.WPF/Converters/VisibilityConverter.cs
+++ src/MH.UI.WPF/Converters/VisibilityConverter.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Windows;
 using System.Windows.Data;
 
 namespace MH.UI.WPF.Converters;
 
-public enum CheckFor { NotNull, Null, True, False, NotEmpty, NullOrEmpty, MoreThan0, EqualsParam, NotEqualsParam }
+public enum CheckFor { NotNull, Null, True, False, NotEmpty, NullOrEmpty, MoreThan0, EqualsParam, NotEqualsParam, Type }
 
 public class VisibilityConverter : BaseConverter {
   private static VisibilityConverter? _nullToVisible;
@@ -17,6 +18,7 @@ public class VisibilityConverter : BaseConverter {
   private static VisibilityConverter? _intToVisible;
   private static VisibilityConverter? _equalsParamToVisible;
   private static VisibilityConverter? _notEqualsParamToVisible;
+  private static VisibilityConverter? _typeToVisible;
 
   public static VisibilityConverter NullToVisible => _nullToVisible ??= new() { CheckFor = CheckFor.Null, ToVisible = true };
   public static VisibilityConverter NotNullToVisible => _notNullToVisible ??= new() { CheckFor = CheckFor.NotNull, ToVisible = true };
@@ -28,6 +30,7 @@ public class VisibilityConverter : BaseConverter {
   public static VisibilityConverter IntToVisible => _intToVisible ??= new() { CheckFor = CheckFor.MoreThan0, ToVisible = true };
   public static VisibilityConverter EqualsParamToVisible => _equalsParamToVisible ??= new() { CheckFor = CheckFor.EqualsParam, ToVisible = true };
   public static VisibilityConverter NotEqualsParamToVisible => _notEqualsParamToVisible ??= new() { CheckFor = CheckFor.NotEqualsParam, ToVisible = true };
+  public static VisibilityConverter TypeToVisible => _typeToVisible ??= new() { CheckFor = CheckFor.Type, ToVisible = true };
 
   public CheckFor CheckFor { get; init; }
 
@@ -37,19 +40,20 @@ public class VisibilityConverter : BaseConverter {
 
   public override object Convert(object? value, object? parameter) =>
     CheckFor switch {
-      CheckFor.NotNull => GetFor(value != null),
-      CheckFor.Null => GetFor(value == null),
-      CheckFor.True => GetFor(value is true),
-      CheckFor.False => GetFor(value is false),
-      CheckFor.NotEmpty => GetFor((value is string s && !string.IsNullOrEmpty(s)) || (value is IList { Count: > 0 })),
-      CheckFor.NullOrEmpty => GetFor(value is null || string.IsNullOrEmpty(value as string) || value is IList { Count: 0 }),
-      CheckFor.MoreThan0 => GetFor(value is > 0),
-      CheckFor.EqualsParam => GetFor(Equals(value, parameter)),
-      CheckFor.NotEqualsParam => GetFor(!Equals(value, parameter)),
+      CheckFor.NotNull => _getFor(value != null),
+      CheckFor.Null => _getFor(value == null),
+      CheckFor.True => _getFor(value is true),
+      CheckFor.False => _getFor(value is false),
+      CheckFor.NotEmpty => _getFor((value is string s && !string.IsNullOrEmpty(s)) || (value is IList { Count: > 0 })),
+      CheckFor.NullOrEmpty => _getFor(value is null || string.IsNullOrEmpty(value as string) || value is IList { Count: 0 }),
+      CheckFor.MoreThan0 => _getFor(value is > 0),
+      CheckFor.EqualsParam => _getFor(Equals(value, parameter)),
+      CheckFor.NotEqualsParam => _getFor(!Equals(value, parameter)),
+      CheckFor.Type => _getFor(_isTypeOf(value, parameter)),
       _ => Binding.DoNothing
     };
 
-  private Visibility GetFor(bool flag) {
+  private Visibility _getFor(bool flag) {
     if (flag) {
       if (ToVisible) return Visibility.Visible;
       if (ToCollapsed) return Visibility.Collapsed;
@@ -62,4 +66,9 @@ public class VisibilityConverter : BaseConverter {
 
     return Visibility.Collapsed;
   }
+
+  private bool _isTypeOf(object? value, object? parameter) =>
+    value != null && parameter is Type pType && (pType.IsInterface
+      ? value.GetType().GetInterface(pType.Name) != null
+      : value.GetType().IsAssignableTo(pType));
 }
