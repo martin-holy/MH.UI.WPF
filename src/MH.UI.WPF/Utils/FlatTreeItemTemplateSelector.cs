8985debcbe325ad6c56172bbb4d99952fecd0443
martin-holy <martin.holy@hotmail.com>
2026-06-28 20:45:48 +0200

    [N] FlatTreeItemTemplateSelector

---
+++ src/MH.UI.WPF/Utils/FlatTreeItemTemplateSelector.cs
@@ -0,0 +1,13 @@
+﻿using MH.Utils.BaseClasses;
+using System.Windows;
+
+namespace MH.UI.WPF.Utils;
+
+public class FlatTreeItemTemplateSelector : TypeDataTemplateSelector {
+  public override DataTemplate? SelectTemplate(object? item, DependencyObject container) {
+    if (item is FlatTreeItem flatTreeItem)
+      return base.SelectTemplate(flatTreeItem.TreeItem, container);
+
+    return base.SelectTemplate(item, container);
+  }
+}
